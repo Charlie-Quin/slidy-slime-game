e24c7cb84fef6f73f4883fd2202aec0de8baabca
jonathan-quin <112405329+jonathan-quin@users.noreply.github.com>
2024-01-22 14:02:35 -0500

    removed most of the code and made it work

--- blob.gd
+++ blob.gd
@@ -5,15 +5,13 @@ class_name blobClass
 var fixed = false
 
 const STEPSIZE = 32
-const DEBUG = true
+const DEBUG = false
 const DEBUGTIME = 0.05
 var parentConglomerate
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	lastPos = position
-	
-	$Label.text = str(randi() % 1000)
 	
 	pass # Replace with function body.
 
@@ -38,22 +36,6 @@ func updateCurrentlyOnWall(direction):
 	raycast.target_position = direction * STEPSIZE 
 	raycast.force_raycast_update()
 	currentlyTouchingWall = raycast.is_colliding()
-	#
-	#raycast = $blobcast as RayCast2D
-	#raycast.global_position = global_position
-	#raycast.target_position = direction * STEPSIZE 
-	#raycast.force_raycast_update()
-	#currentlyTouchingBlob = raycast.is_colliding()
-	#
-	#
-	#blobInFront = raycast.get_collider()
-	#
-	#raycast.target_position *= -1
-	#raycast.force_raycast_update()
-	#blobBehind = raycast.get_collider()
-	#
-	#raycast.target_position *= -1
-	#raycast.force_raycast_update()
 	
 	blobInFront = getBlobInPosition(position + direction * STEPSIZE)
 	blobBehind = getBlobInPosition(position + direction * -STEPSIZE)
@@ -62,12 +44,6 @@ func updateCurrentlyOnWall(direction):
 #this function can fix blobs.
 func tryMoveMeAndMyBuddies(direction):
 	
-	if fixed:
-		$Blob.scale = Vector2.ONE * 1.5
-	else:
-		$Blob.scale = Vector2.ONE
-	
-	#buddies includes yourself
 	var buddies = [self]
 	
 	var currentBlob = self
@@ -98,97 +74,48 @@ func tryMoveMeAndMyBuddies(direction):
 		if buddy.fixed:
 			cannotMove = true
 	
-	for buddy in buddies:
-		buddy.scale = Vector2.ONE * 2
-	scale = Vector2.ONE * 3
-	pass
-	for buddy in buddies:
-		buddy.scale = Vector2.ONE * 1
-	
-	
 	if cannotMove:
 		return false
 	
 	var startedNearFixed = false
 	var fixedsWeStartedNear = []
 	
+	var startedNearAnything = false
+	var anythingWeStartedNear = []
+	
 	for blob in getAllBlobsConnectedToSelf():
+		startedNearAnything = true
+		anythingWeStartedNear.append(blob)
 		if blob.fixed:
 			fixedsWeStartedNear.append(blob)
 			startedNearFixed = true
 		
-		
-		#for neighbor in buddy.getAdjacentBlobs():
-			#if neighbor.fixed:
-				#startedNearFixed = true
-				#fixedsWeStartedNear.append(neighbor)
-				#break
 	
-	var numMoved = 0
+	
 	
 	for buddy in buddies:
-		print("moving ",str(numMoved))
-		numMoved += 1
 		buddy.move(direction)
 	
-	print("finished moving" )
 	
-	if startedNearFixed:
+	
+	if startedNearAnything:
 		
-		var touchingFixed = false
-		
-		for blob in getAllBlobsConnectedToSelf():
-			
-			if blob.fixed:
-				touchingFixed = true
+		var leftSomethingAlone = false
+		var connectedBlobs = getAllBlobsConnectedToSelf()
+		for blob in anythingWeStartedNear:
+			if !connectedBlobs.has(blob):
+				leftSomethingAlone = true
 				break
-			
 		
-		if !touchingFixed:
-			pass
-			for buddy in buddies:
-				buddy.move(-direction)
-			return false
-		
-		#var touchingFixed = false
-		#
-		#for buddy in buddies:
-			#
-			#for neighbor in buddy.getAdjacentBlobs():
-				#
-				#if neighbor.fixed:
-					#
-					#touchingFixed = true
-					#break
-			#
-			#if touchingFixed:
-				#break;
-			#
-		#
-		#if !touchingFixed:
-			#pass
-			#for buddy in buddies:
-				#buddy.move(-direction)
-			#return false
-		#
-		var leftFixedAlone = false
-		
-		for blob in fixedsWeStartedNear:
-			if !blob.getAllBlobsConnectedToSelf().has(self):
-				leftFixedAlone = true
-		
-		if leftFixedAlone:
+		if leftSomethingAlone:
 			for buddy in buddies:
 				buddy.move(-direction)
 			return false
 		
 		
-	
-	print("finished getting away from fixed")
 	
 	return true
 	
-	pass
 
 func getAllBlobsConnectedToSelf():
 	var allBlobs = parentConglomerate.blobs
@@ -229,71 +156,12 @@ func getAllBlobsConnectedToSelf():
 #this function can't fix the blobs
 func move(direction):
 	
-	lastPos = position
-	lastDir = direction
-	
-	print(position)
 	position += direction * STEPSIZE
-	print(position)
 	
 	pass
 
-func tryMove(direction):
-	if DEBUG:
-		await get_tree().create_timer(DEBUGTIME).timeout
-	
-	lastPos = position
-	lastDir = direction
-	
-	updateCurrentlyOnWall(direction)
-	
-	if currentlyTouchingWall:
-		return false
-	
-	position += direction * STEPSIZE
-	
-	updateCurrentlyOnWall(direction)
-	
-	return true
 
-func stepBackIfOverlapping():
-	if DEBUG:
-		await get_tree().create_timer(DEBUGTIME).timeout
-	for blob in parentConglomerate.blobs:
-		
-		if blob == self:
-			continue
-		
-		if blob.position.is_equal_approx(position):
-			position -= lastDir * STEPSIZE
-			currentlyTouchingWall = false
-			return true;
-		
-	return false
 
-func goBack():
-	position = lastPos
-
-func stepBack():
-	if DEBUG:
-		await get_tree().create_timer(DEBUGTIME).timeout
-	position -= lastDir * STEPSIZE
-
-func checkIfPositionIsValid():
-	
-	for blob in parentConglomerate.blobs:
-		
-		if blob == self:
-			continue
-		
-		if blob.position.is_equal_approx(position):
-			return false;
-		
-		if blob.position.distance_to(position) < STEPSIZE * 1.5:
-			return true
-		
-	
-	return false
 
 func getAdjacentBlobs():
 	
--- conglomerate.gd
+++ conglomerate.gd
@@ -30,34 +30,13 @@ func _process(delta):
 
 func moveBlobs(direction):
 	
-	var compress = true
-	
 	for blob in blobs:
 		
 		blob.scale = Vector2.ONE
 		blob.fixed = false
 		blob.updateCurrentlyOnWall(direction)
 		
-		if blob.currentlyTouchingWall:
-			compress = false
-		
 	
-	
-	
-	#compress
-	while compress:
-		var shouldBreak = false
-		
-		for blob in blobs:
-			blob = blob as blobClass
-			await blob.tryMove(direction)
-			if blob.currentlyTouchingWall:
-				shouldBreak = true
-				blob.fixed = true
-		
-		if shouldBreak:
-			break
-		
 	
 	print("finished moving buddies")
 	
@@ -66,157 +45,18 @@ func moveBlobs(direction):
 		
 		for blob in blobs:
 			
-			if await blob.tryMoveMeAndMyBuddies(direction):
+			if blob.tryMoveMeAndMyBuddies(direction):
 				somethingMoved = true
 			
 		
 		if !somethingMoved:
 			break
+	
 	
 	running = false
 	return
-	#decompress
-	while true:
-		var somethingMoved = false
-		
-		for blob in blobs:
-			
-			if await blob.stepBackIfOverlapping():
-				somethingMoved = true
-			
-		
-		if !somethingMoved:
-			break
-	
-	var leastMovedBlob = findNegativeDirectionMostBlobThatIsTouchingWall(direction)
-	var currentFocusBlob = leastMovedBlob
-	#fix everything to the right
-	while true:
-		var rightBlobs = getBlobsRelativeToBlob(direction,currentFocusBlob,true)
-		
-		if rightBlobs.size() == 0:
-			break
-		
-		while true:
-			if checkIfAnyBlobIsTouching(rightBlobs,currentFocusBlob):
-				for blob in rightBlobs:
-					if blob.currentlyTouchingWall:
-						currentFocusBlob = blob
-				break
-			for blob in rightBlobs:
-				await blob.stepBack()
-			
-	
-	#fix to the left
-	currentFocusBlob = leastMovedBlob
-	while true:
-		var leftBlobs = getBlobsRelativeToBlob(direction,currentFocusBlob,false)
-		
-		if leftBlobs.size() == 0:
-			break
-		
-		while true:
-			if checkIfAnyBlobIsTouching(leftBlobs,currentFocusBlob):
-				for blob in leftBlobs:
-					if blob.currentlyTouchingWall:
-						currentFocusBlob = blob
-				break
-			for blob in leftBlobs:
-				await blob.stepBack()
-			
-	
-	running = false
 	
 
-
-
-func checkIfAnyBlobIsTouching(inputBlobs,targetBlob):
-	
-	for blob in inputBlobs:
-		
-		if blob.position.distance_to(targetBlob.position) < STEPSIZE * 1.5:
-			return true
-		
-	
-	return false
-	
-
-func getBlobsRelativeToBlob( direction,inputBlob,right):
-	
-	var returnBlobs = []
-	
-	if direction == Vector2.UP or direction == Vector2.DOWN:
-		var desiredX = inputBlob.position.x + STEPSIZE if right else inputBlob.position.x - STEPSIZE
-		
-		for blob in blobs:
-			if is_equal_approx(blob.position.x,desiredX):
-				returnBlobs.append(blob)
-	else:
-		var desiredY = inputBlob.position.y + STEPSIZE if right else inputBlob.position.y - STEPSIZE
-		
-		for blob in blobs:
-			if is_equal_approx(blob.position.y,desiredY):
-				returnBlobs.append(blob)
-	
-	
-	return returnBlobs
-	pass
-
-func findNegativeDirectionMostBlobThatIsTouchingWall(direction):
-	
-	var best = null
-	var bestBlob = null
-	
-	if direction == Vector2.RIGHT:
-		for blob in blobs:
-			
-			if blob.currentlyTouchingWall == false:
-				continue
-			
-			pass
-			
-			if best == null or blob.position.x < best:
-				best = blob.position.x
-				bestBlob = blob
-	if direction == Vector2.LEFT:
-		for blob in blobs:
-			if blob.currentlyTouchingWall == false:
-				continue
-			
-			if best == null or blob.position.x > best:
-				best = blob.position.x
-				bestBlob = blob
-	if direction == Vector2.UP:
-		for blob in blobs:
-			if blob.currentlyTouchingWall == false:
-				continue
-			
-			if best == null or blob.position.y > best:
-				best = blob.position.y
-				bestBlob = blob
-	if direction == Vector2.DOWN:
-		for blob in blobs:
-			if blob.currentlyTouchingWall == false:
-				continue
-			
-			if best == null or blob.position.y < best:
-				best = blob.position.y
-				bestBlob = blob
-	
-	return bestBlob
-	
-	pass
-
-func checkIfBlobStateIsValid():
-	
-	for blob in blobs:
-		if !blob.checkIfPositionIsValid():
-			return false;
-	
-	return true
-	
-	
-	pass
 
 func setBlobs():
 	blobs.clear()
